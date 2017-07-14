274aae607fdc3b97f821c2f13c649d1beedf12ed
purelledhand <wergreat10@naver.com>
2017-07-14 11:14:26 -0700

    make json format, file I/O, close #1, #2

--- whois.py
+++ whois.py
@@ -1,11 +1,25 @@
+from json import dumps
+from datetime import date, datetime
 import pythonwhois
 import pdb
+import json
 
-f = open('url_sample.txt', 'r')
-line = f.readlines()
+def json_serial(obj):
+    if isinstance(obj, (datetime, date)):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+urlfile = open('url_sample.txt', 'r')
+line = urlfile.readlines()
+
+whoisfile = open('result.txt','w')
 
 for i in line:
-        print i
-        print pythonwhois.get_whois(i)
+  print i
+  tmp = str(pythonwhois.get_whois(i))
+  whoisfile.write(tmp)
+  print json.dumps(tmp, default=json_serial)
 
-
+urlfile.close()
+whoisfile.close()
